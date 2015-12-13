2eb1042dd1d72747f0289d580c4088bae25a9e29
Ikeda Iori <m134_warrock_rpg7@yahoo.com>
2015-12-13 13:09:07 +0900

    Add AuthenticationPlug

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,7 +1,22 @@
 defmodule Xee3rd.PageController do
   use Xee3rd.Web, :controller
+  require Logger
 
   def index(conn, _params) do
     render conn, "index.html"
+  end
+
+  def themes(conn, _params) do
+    test
+    themes = Xee3rd.ThemeServer.get_all
+              |> Map.to_list
+              |> Enum.map(fn {_key, value} -> value end)
+    render conn, "themes.html", themes: themes
+  end
+
+  defp test() do
+    Logger.debug inspect Xee3rd.ThemeServer.get_all
+    Xee3rd.ThemeServer.add :test,  %{name: "Ricardo Comparison Production Theory"}
+    Xee3rd.ThemeServer.add :test1, %{name: "Double Auction"}
   end
 end
