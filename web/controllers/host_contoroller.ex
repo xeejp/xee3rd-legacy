7f8822bbb9537560d6a1c908ec1a0f611f5880dc
Ikeda Iori <m134_warrock_rpg7@yahoo.com>
2015-12-20 13:05:05 +0900

    Change lang and Modify experiments.html and host/index.html

--- web/controllers/host_contoroller.ex
+++ web/controllers/host_contoroller.ex
@@ -1,10 +1,13 @@
 defmodule Xee3rd.HostController do
   use Xee3rd.Web, :controller
+  require Logger
 
   plug Xee3rd.AuthenticationPlug
 
   def index(conn, _params) do
-    render conn, "index.html"
+    experiment = Xee3rd.HostServer.get(get_session(conn, :current_user))
+    Logger.debug inspect experiment
+    render conn, "index.html", experiment: experiment
   end
 
   def experiments(conn, _params) do
@@ -14,14 +17,22 @@ defmodule Xee3rd.HostController do
     render conn, "experiments.html", csrf_token: get_csrf_token(), experiment_name: "", theme_num: 0, themes: themes, id: Xee3rd.Experiment.generate_id#id: Xee3rd.ExperimentServer.generate_id
   end
 
-  #def create(conn, %{"_csrf_token" => token, "experimentName" => name, "experimentTheme" => theme}) do
-    def create(conn, %{"experiment_name" => name, "theme" => theme, "id" => id}) do
-    # 入力チェック　（エラーならindex.htmlにエラーをつけて返す)
-    if (name == nil) do
-      render conn, "index.html", error: "Create Experiment Error!"
-    else
-      #Xee.3rd.HostServer.register(theme, Xee3rd.ExperimentServer.create_experiment(key, experiment_id, experiment))
-      #conn |> redirect(Router.root_path) |> id
+  def create(conn, %{"experiment_name" => name, "theme" => theme, "id" => id}) do
+      if (name == nil || name == "") do
+        conn
+        |> put_flash(:error, "Make Experiment Error")
+        |> redirect(to: "/host")
+      else
+        if Xee3rd.HostServer.get(get_session(conn, :current_user)) do
+          Xee3rd.HostServer.drop(get_session(conn, :current_user))
+        end
+        uid = Xee3rd.ExperimentServer.create(id, name)
+        experiment_info = %{uid: uid, name: name, theme: theme, x_id: id}
+        Xee3rd.HostServer.register(get_session(conn, :current_user), experiment_info)
+        conn
+        |> put_flash(:info, "Made New Experiment : " <> theme)
+        |> redirect(to: "/experiment/" <> id <> "/host")
+      end
     end
     render conn, "index.html"
   end
--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -16,7 +16,9 @@ defmodule Xee3rd.PageController do
 
   defp test() do
     Logger.debug inspect Xee3rd.ThemeServer.get_all
-    Xee3rd.ThemeServer.add :test,  %{name: "Ricardo Comparison Production Theory"}
-    Xee3rd.ThemeServer.add :test1, %{name: "Double Auction"}
+    Xee3rd.ThemeServer.add :test,  %{name: "リカード生産比較", playnum: "2235", lastupdate: "2015/1/1", producer: "hoge", contact: "aaa@aaa", manual: ""}
+    Xee3rd.ThemeServer.add :test1, %{name: "ダブルオークション実験", playnum: "345", lastupdate: "2015/1/2", producer: "piyo", contact: "bbb@bbb", manual: "#"}
+    Xee3rd.ThemeServer.add :test2, %{name: "公共財実験", playnum: "678", lastupdate: "2015/12/1", producer: "foo", contact: "ccc@ccc", manual: "#"}
+    Xee3rd.ThemeServer.add :test3, %{name: "アンケート実験", playnum: "37544",lastupdate: "2015/7/31", producer: "bar", contact: "ddd@ddd", manual: ""}
   end
 end
