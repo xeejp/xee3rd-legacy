4ac7eebed5f29e65c9525140c8303972a50d3b27
Ikeda Iori <m134_warrock_rpg7@yahoo.com>
2015-12-24 13:36:10 +0900

    Modify HostPage and HostController

--- web/controllers/host_contoroller.ex
+++ web/controllers/host_contoroller.ex
@@ -6,7 +6,6 @@ defmodule Xee3rd.HostController do
 
   def index(conn, _params) do
     experiment = Xee3rd.HostServer.get(get_session(conn, :current_user))
-    Logger.debug inspect experiment
     render conn, "index.html", experiment: experiment
   end
 
@@ -14,24 +13,32 @@ defmodule Xee3rd.HostController do
     themes = Xee3rd.ThemeServer.get_all
               |> Map.to_list
               |> Enum.map(fn {_key, value} -> value end)
-    render conn, "experiments.html", csrf_token: get_csrf_token(), experiment_name: "", theme_num: 0, themes: themes, id: Xee3rd.Experiment.generate_id#id: Xee3rd.ExperimentServer.generate_id
+    render conn, "experiments.html", csrf_token: get_csrf_token(), experiment_name: "", theme_num: "0", themes: themes, user_num: "1", startDateTime: "", endDateTime: "", showDescription: "true", id: Xee3rd.Experiment.generate_id
   end
 
-  def create(conn, %{"experiment_name" => name, "theme" => theme, "id" => id}) do
+  def create(conn, %{"experiment_name" => name, "theme" => theme, "user_num" => user_num, "startDateTime" => start_info, "endDateTime" => end_info, "showDescription" => show, "x_id" => x_id}) do
     if (name == nil || name == "") do
       conn
       |> put_flash(:error, "Make Experiment Error")
       |> redirect(to: "/host")
     else
-      if Xee3rd.HostServer.get(get_session(conn, :current_user)) do
-        Xee3rd.HostServer.drop(get_session(conn, :current_user))
+      has   = Xee3rd.HostServer.has?(get_session(conn, :current_user), x_id)
+      exist = Xee3rd.ExperimentServer.has?(x_id)
+      unless (has || exist) do
+        uid = Xee3rd.ExperimentServer.create(x_id, name)
+        experiment_info = %{uid: uid, name: name, theme: theme, user_num: user_num, start_info: start_info, end_info: end_info, show: show, x_id: x_id}
+        Xee3rd.HostServer.register(get_session(conn, :current_user), experiment_info)
+        conn
+        |> put_flash(:info, "Made New Experiment : " <> theme)
+        |> redirect(to: "/experiment/" <> x_id <> "/host")
+      else
+        themes = Xee3rd.ThemeServer.get_all
+              |> Map.to_list
+              |> Enum.map(fn {_key, value} -> value end)
+        conn
+        |> put_flash(:error, "This ExperimentID is already being used.")
+        |> render "experiments.html", csrf_token: get_csrf_token(), experiment_name: name, theme_num: theme, themes: themes, user_num: user_num, startDateTime: start_info, endDateTime: end_info, showDescription: show, id: Xee3rd.Experiment.generate_id
       end
-      uid = Xee3rd.ExperimentServer.create(id, name)
-      experiment_info = %{uid: uid, name: name, theme: theme, x_id: id}
-      Xee3rd.HostServer.register(get_session(conn, :current_user), experiment_info)
-      conn
-      |> put_flash(:info, "Made New Experiment : " <> theme)
-      |> redirect(to: "/experiment/" <> id <> "/host")
     end
   end
 end
